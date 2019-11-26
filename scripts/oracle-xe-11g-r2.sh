7fb9638e48cbb584ddd5ed994d7b5b329911a3f9
Jmnote <opcore@gmail.com>
2019-11-26 15:53:47 +0900

    Update and rename docker-oracle-xe-11g-r2.sh to oracle-xe-11g-r2.sh

--- scripts/oracle-xe-11g-r2.sh
+++ scripts/oracle-xe-11g-r2.sh
@@ -1,10 +1,9 @@
 set -ax
-rm -rf /tmp/jmnote-docker-images
-mkdir /tmp/jmnote-docker-images
-cd /tmp/jmnote-docker-images
+rm -rf /tmp/jmnote-docker-images; mkdir /tmp/jmnote-docker-images; cd /tmp/jmnote-docker-images
+
 git clone --depth=1 https://github.com/wnameless/docker-oracle-xe-11g.git
 cd docker-oracle-xe-11g
 docker build -t jmnote/oracle-xe-11g-r2:11.2.0 .
 docker push jmnote/oracle-xe-11g-r2:11.2.0
-rm -rf /tmp/jmnote-docker-images
 
+rm -rf /tmp/jmnote-docker-images
