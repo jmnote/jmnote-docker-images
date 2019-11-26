b6cd681a416cccb2328e8ebceb18d670d746239f
Jmnote <opcore@gmail.com>
2019-11-26 15:03:25 +0900

    Update docker-oracle-xe-11g.sh

--- scripts/docker-oracle-xe-11g.sh
+++ scripts/docker-oracle-xe-11g.sh
@@ -1,5 +1,5 @@
 set -ax
 git clone https://github.com/wnameless/docker-oracle-xe-11g.git
 cd docker-oracle-xe-11g
-docker build -t jmnote/oracle-xe-11g .
-docker push jmnote/oracle-xe-11g .
+docker build -t jmnote/oracle-xe-11g:11.2.0 .
+docker push jmnote/oracle-xe-11g:11.2.0 .
