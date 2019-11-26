5087e2ee2de891b4abca4e83eaba52534b844477
Jmnote <opcore@gmail.com>
2019-11-26 16:17:28 +0900

    Update weblogic.sh

--- scripts/weblogic.sh
+++ scripts/weblogic.sh
@@ -3,6 +3,6 @@ rm -rf /tmp/jmnote-docker-images; mkdir /tmp/jmnote-docker-images; cd /tmp/jmnot
 
 git clone --depth=1 https://github.com/oracle/docker-images.git
 cd docker-images/OracleWebLogic/dockerfiles/
-sh buildDockerImage.sh -d
+. /buildDockerImage.sh -d
 
 rm -rf /tmp/jmnote-docker-images
