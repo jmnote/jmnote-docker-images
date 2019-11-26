887cbeab76cc9b7dd998282843be15dcfc677028
jmnote <opcore@gmail.com>
2019-11-26 15:33:52 +0900

    .

--- scripts/docker-oracle-xe-11g.sh
+++ scripts/docker-oracle-xe-11g.sh
@@ -1,5 +1,10 @@
 set -ax
-git clone https://github.com/wnameless/docker-oracle-xe-11g.git
+rm -rf /tmp/jmnote-docker-images
+mkdir /tmp/jmnote-docker-images
+cd /tmp/jmnote-docker-images
+git clone --depth=1 https://github.com/wnameless/docker-oracle-xe-11g.git
 cd docker-oracle-xe-11g
 docker build -t jmnote/oracle-xe-11g:11.2.0 .
-docker push jmnote/oracle-xe-11g:11.2.0 .
+docker push jmnote/oracle-xe-11g:11.2.0
+rm -rf /tmp/jmnote-docker-images
+
