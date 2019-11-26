2610fa0573095b389141c49d53dfe8fbe14fff5f
Jmnote <opcore@gmail.com>
2019-11-26 16:09:19 +0900

    Create weblogic.sh

---
+++ scripts/weblogic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf /tmp/jmnote-docker-images; mkdir /tmp/jmnote-docker-images; cd /tmp/jmnote-docker-images
+
+git clone --depth=1 https://github.com/oracle/docker-images.git
+cd docker-images/OracleWebLogic/dockerfiles/
+sh buildDockerImage.sh -d
+
+rm -rf /tmp/jmnote-docker-images
