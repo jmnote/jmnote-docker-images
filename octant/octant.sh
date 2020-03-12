546a8ce5fbfeffc7667ca4e40cfb1cb3fd1023ed
jmnote <opcore@gmail.com>
2020-03-12 14:27:56 +0900

    .

--- octant/octant.sh
+++ octant/octant.sh
@@ -3,7 +3,7 @@ rm -rf /tmp/jmnote-docker-images
 mkdir /tmp/jmnote-docker-images
 cd /tmp/jmnote-docker-images
 
-IMAGE=jmnote/octant:0.10.2-debian-slim
+IMAGE=jmnote/octant:0.10.2
 
 curl -o octant.deb -L https://github.com/vmware-tanzu/octant/releases/download/v0.10.2/octant_0.10.2_Linux-64bit.deb
 
