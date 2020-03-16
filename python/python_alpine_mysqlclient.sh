01f265f23f8ca6f70e6cd1efdb0c648bd291a669
jmnote <opcore@gmail.com>
2020-03-16 10:08:30 -0400

    .

--- python/python_alpine_mysqlclient.sh
+++ python/python_alpine_mysqlclient.sh
@@ -5,10 +5,9 @@ cd /tmp/jmnote-docker-images
 
 cat <<'EOF' > Dockerfile
 FROM python:3.8.2-alpine3.11
-RUN apk add --no-cache mariadb-dev g++ \
+RUN apk add --no-cache mariadb-connector-c-dev build-base \
 && pip install --no-cache mysqlclient \
-&& apk del g++ mariadb-dev \
-&& apk add --no-cache mariadb-client
+&& apk del build-base
 EOF
 
 docker build -t jmnote/python:3.8.2-alpine3.11-mysqlclient .
