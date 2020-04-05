581d84654a3c9374055a5ee141612f3b5a3935b7
jmnote <opcore@gmail.com>
2020-04-05 02:15:20 -0400

    .

---
+++ python/python-slim_supervisor_openssh-server.sh
@@ -0,0 +1,19 @@
+set -x
+rm -rf /tmp/jmnote-docker-images
+mkdir /tmp/jmnote-docker-images
+cd /tmp/jmnote-docker-images
+
+IMAGE=jmnote/python:3.8.2-slim_supervisor_openssh-server
+
+cat <<'EOF' > Dockerfile
+FROM python:3.8.2-slim
+RUN set -xe \
+&& apt-get update -y \
+&& apt-get install -y --no-install-recommends \
+  supervisor \
+  openssh-server \
+&& rm -rf /var/lib/apt/lists/*
+EOF
+
+docker build -t $IMAGE . && docker push $IMAGE
+
