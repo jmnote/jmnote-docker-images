cca37ec7373d7ac012bb83ac54324781b0292578
jmnote <opcore@gmail.com>
2020-04-08 06:03:12 -0400

    .

--- python/python-alpine_mysqlclient_google-cloud-storage.sh
+++ python/python-alpine_mysqlclient_google-cloud-storage.sh
@@ -4,12 +4,12 @@ mkdir /tmp/jmnote-docker-images
 cd /tmp/jmnote-docker-images
 
 cat <<'EOF' > Dockerfile
-FROM python:3.8.2-alpine3.11
+FROM python:3.8.2-alpine
 RUN apk add --no-cache mariadb-connector-c-dev build-base \
 && pip install --no-cache mysqlclient google-cloud-storage \
 && apk del build-base
 EOF
 
-docker build -t jmnote/python:3.8.2-alpine3.11-mysqlclient-google-cloud-storage .
-docker push jmnote/python:3.8.2-alpine3.11-mysqlclient-google-cloud-storage
+docker build -t jmnote/python:3.8.2-alpine-mysqlclient-google-cloud-storage .
+docker push jmnote/python:3.8.2-alpine-mysqlclient-google-cloud-storage
 
