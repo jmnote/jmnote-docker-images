749724fdffd579802033406b5815d8b2ef1ea345
jmnote <opcore@gmail.com>
2020-10-24 12:25:54 +0000

    .

--- pywikibot/pywikibot.sh
+++ pywikibot/pywikibot.sh
@@ -5,7 +5,7 @@ cd /tmp/jmnote-docker-images
 
 TAG=5.0.0
 
-git clone --depth 1 -b $TAG https://github.com/wikimedia/pywikibot.git
+git clone --depth 1 --recursive -b $TAG https://github.com/wikimedia/pywikibot.git
 cd pywikibot/
 
 docker build -t jmnote/pywikibot:$TAG . && docker push jmnote/pywikibot:$TAG
