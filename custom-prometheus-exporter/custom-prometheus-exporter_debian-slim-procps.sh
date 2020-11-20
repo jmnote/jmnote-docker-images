5768e241638b23f0b77cdc141c3d61d419b5ee4f
Jmnote <opcore@gmail.com>
2020-11-20 16:35:51 +0900

    Update custom-prometheus-exporter_debian-slim-procps.sh

--- custom-prometheus-exporter/custom-prometheus-exporter_debian-slim-procps.sh
+++ custom-prometheus-exporter/custom-prometheus-exporter_debian-slim-procps.sh
@@ -6,7 +6,7 @@ cd /tmp/jmnote-docker-images
 git clone --depth=1 https://github.com/marckhouzam/custom-prometheus-exporter.git
 cd custom-prometheus-exporter/
 
-TAG=debian-procps-$(date +%Y-%m)
+TAG=debian-slim-procps-$(date +%Y-%m)
 
 cat <<EOF > Dockerfile
 FROM golang:1.9 as build
