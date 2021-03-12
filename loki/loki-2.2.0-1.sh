347974908456ec06614c305c136e3e3373089c78
jmnote <opcore@gmail.com>
2021-03-12 09:46:43 +0000

    .

---
+++ loki/loki-2.2.0-1.sh
@@ -0,0 +1,16 @@
+set -x
+rm -rf /tmp/jmnote-docker-images
+mkdir /tmp/jmnote-docker-images
+cd /tmp/jmnote-docker-images
+
+IMAGE=jmnote/loki:2.2.0-1
+
+cat <<'EOF' > Dockerfile
+FROM grafana/loki:2.2.0
+USER root
+RUN apk --no-cache add curl
+USER loki
+EOF
+
+docker build -t $IMAGE . && docker push $IMAGE
+
--- loki/loki-2.2.0.sh
+++ loki/loki-2.2.0.sh
@@ -3,13 +3,10 @@ rm -rf /tmp/jmnote-docker-images
 mkdir /tmp/jmnote-docker-images
 cd /tmp/jmnote-docker-images
 
-IMAGE=jmnote/loki:2.2.0-0
+IMAGE=jmnote/loki:2.2.0
 
 cat <<'EOF' > Dockerfile
 FROM grafana/loki:2.2.0
-USER root
-RUN apk --no-cache add curl
-USER loki
 EOF
 
 docker build -t $IMAGE . && docker push $IMAGE
