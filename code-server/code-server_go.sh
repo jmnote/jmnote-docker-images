c6415c5daa0ee8663aa78a16e7dcaa62c6cb595b
Jmnote <opcore@gmail.com>
2021-04-15 14:07:23 +0900

    Update code-server_go.sh

--- code-server/code-server_go.sh
+++ code-server/code-server_go.sh
@@ -26,7 +26,8 @@ RUN set -x \
 && curl -L https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl \
 && chmod +x /usr/local/bin/kubectl \
 && curl -LO https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz \
-&& tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz
+&& tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz \
+&& rm -f go${GOLANG_VERSION}.linux-amd64.tar.gz
 
 USER coder
 RUN set -x \
