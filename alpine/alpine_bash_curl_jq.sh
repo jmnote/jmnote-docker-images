2a6cec97b816dc673cae75a7cf6cf13b42f23ee2
jmnote <opcore@gmail.com>
2021-06-01 01:58:40 +0000

    .

---
+++ alpine/alpine_bash_curl_jq.sh
@@ -0,0 +1,22 @@
+set -x
+rm -rf /tmp/jmnote-docker-images
+mkdir /tmp/jmnote-docker-images
+cd /tmp/jmnote-docker-images
+
+BASE=alpine:3.13.5
+PACKAGES=(
+bash
+curl
+jq
+)
+
+IMAGE=$(echo "jmnote/$BASE-${PACKAGES[@]}" | sed 's/ /-/g')
+echo IMAGE=$IMAGE
+
+cat <<EOF > Dockerfile
+FROM $BASE
+RUN apk add --no-cache ${PACKAGES[@]}
+EOF
+
+docker build -t $IMAGE . && \
+docker push     $IMAGE
