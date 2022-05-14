8e264918343f459224eb03800072c326daad89c8
Jmnote <opcore@gmail.com>
2022-05-15 03:31:49 +0900

    Update code-server_go.sh

--- code-server/code-server_go.sh
+++ code-server/code-server_go.sh
@@ -6,40 +6,22 @@ cd /tmp/jmnote-docker-images
 IMAGE=jmnote/code-server:go
 
 cat <<'EOF' > Dockerfile
-FROM codercom/code-server
+FROM linuxserver/code-server:4.4.0
+RUN DOCKER_MODS=linuxserver/mods:code-server-golang-1.18.2 /init exit; exit 0
+RUN DOCKER_MODS=linuxserver/mods:code-server-nodejs-16     /init exit; exit 0
+RUN install-extension golang.go
+RUN install-extension vue.volar
 
-ENV GOLANG_VERSION=1.16.3
-ENV GOPATH=/root/go
-ENV PATH=$PATH:/usr/local/go/bin
+ENV GOPATH=/go
+ENV GOLANG_VERSION=1.18.2
+ENV PATH=$PATH:/go/bin
 
-USER root
 RUN set -x \
-&& apt-get update && apt-get install -y \
-  jq \
-  vim \
-  curl \
-  tree \
-  netcat \
-  multitail \
-  inotify-tools \
-  openssh-server \
-&& rm -rf /var/lib/apt/lists/* \
-&& curl -L https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl \
-&& chmod +x /usr/local/bin/kubectl \
-&& curl -LO https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz \
-&& tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz \
-&& rm -f go${GOLANG_VERSION}.linux-amd64.tar.gz \
-&& mkdir -p $GOPATH \
-&& cd $GOPATH \
-&& code-server --install-extension golang.go \
-&& go get golang.org/x/tools/gopls \
-&& go get github.com/uudashr/gopkgs/v2/cmd/gopkgs \
-&& go get github.com/ramya-rao-a/go-outline       \
-&& go get github.com/go-delve/delve/cmd/dlv       \
-&& go get honnef.co/go/tools/cmd/staticcheck
+&& go install golang.org/x/tools/gopls@latest \
+&& go intsll github.com/ramya-rao-a/go-outline@latest \
+&& go install github.com/cosmtrek/air@latest
 
-WORKDIR /root/go
+WORKDIR /go
 EOF
 
 docker build -t $IMAGE . && docker push $IMAGE
-
