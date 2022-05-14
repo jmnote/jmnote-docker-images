d9e4f875062212ee6fe187652c7863a005793721
jmnote <opcore@gmail.com>
2022-05-14 19:58:51 +0000

    .

--- code-server/code-server_go.sh
+++ code-server/code-server_go.sh
@@ -8,18 +8,22 @@ IMAGE=jmnote/code-server:go
 cat <<'EOF' > Dockerfile
 FROM linuxserver/code-server:4.4.0
 
+RUN DOCKER_MODS=linuxserver/mods:code-server-golang-1.18.2 /init exit; exit 0
+RUN DOCKER_MODS=linuxserver/mods:code-server-nodejs        /init exit; exit 0
+
 ENV GOPATH=/go
 ENV GOLANG_VERSION=1.18.2
 ENV PATH=$PATH:/usr/local/go/bin:/go/bin
 
 RUN set -x \
-&& DOCKER_MODS=linuxserver/mods:code-server-golang-1.18.2 /init exit; exit 0 \
-&& DOCKER_MODS=linuxserver/mods:code-server-nodejs-16     /init exit; exit 0 \
-&& install-extension golang.go \
-&& install-extension vue.volar \
 && go install golang.org/x/tools/gopls@latest \
 && go install github.com/ramya-rao-a/go-outline@latest \
 && go install github.com/cosmtrek/air@latest
+
+RUN set -x \
+&& install-extension golang.go \
+#&& install-extension vue.volar
+&& install-extension johnsoncodehk.volar
 EOF
 
 docker build -t $IMAGE . && docker push $IMAGE
